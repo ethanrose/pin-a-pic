5cd621255e415ce956da8ece23d2a3454f591bab
Ethan Rose <Ethan Rose>
2017-05-24 23:59:45 -0700

    remove bcrypt (no pass)

--- models/user.js
+++ models/user.js
@@ -6,19 +6,5 @@ var userSchema = new Schema({
 	twitterId: String
 });
 
-userSchema.pre('update', function(next){
-    if (this.getUpdate().$set.password) {
-			var p = this.getUpdate().$set.password
-			p = bcrypt.hashSync(p, 10);
-			this.getUpdate().$set.password = p
-		}
-    next();
-})
-
-userSchema.pre('save', function(next){
-    this.password = bcrypt.hashSync(this.password, 10);
-    next();
-});
-
 var User = mongoose.model('User', userSchema);
 module.exports = User;
