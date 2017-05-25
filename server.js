52aa3023eff212963fa10a1a1eababa9702b49d0
Ethan Rose <Ethan Rose>
2017-05-24 23:50:01 -0700

    fix oauth

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/auth/twitter',
   passport.authenticate('twitter'));
 
 app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  passport.authenticate('twitter'),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
